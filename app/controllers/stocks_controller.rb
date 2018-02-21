64cf721205658495e921217c2d00dc8d420da969
Alika Choo <alikachoo5@gmail.com>
2018-02-21 07:20:50 +0000

    Add user and stock association plus views

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,7 +5,7 @@ class StocksController < ApplicationController
       flash.now[:danger] = "You have entered an empty search string"
     else
       @stock = Stock.new_from_lookup(params[:stock])
-      flash.now[:danger] = "You have entered an incorrect symbol" unless @stock
+      flash.now[:danger] = "You have entered an incorrect symbol" unless @stock #unless @stock is valid
     end
     render partial: "users/result"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'index', to: "welcome#index"
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: 'stocks#search'
+  resources :user_stocks, only: [:create, :destroy]
 end
